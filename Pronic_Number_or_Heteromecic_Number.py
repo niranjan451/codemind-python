b32f3e0f40cf9222ab3337a671affe135343129b
niranjan451 <84759310+niranjan451@users.noreply.github.com>
2022-08-21 22:42:36 +0530

    n=int(input()) l1=[] x=1 y=x+1 for i in range(1,n+1):     if n/x==0 and n%y/0:         break     x=x+1     y=x+1     c=x*y     l1.append(c) if n in l1:
        print("YES")
    else:
        print("NO")

---
+++ Pronic_Number_or_Heteromecic_Number.py
@@ -0,0 +1,17 @@
+n=int(input())
+l1=[]
+x=1
+y=x+1
+for i in range(1,n+1):
+    if n/x==0 and n%y/0:
+        break
+    x=x+1
+    y=x+1
+    c=x*y
+    l1.append(c)
+if n in l1:
+    
+    print("YES")
+else:
+    print("NO")
+    
