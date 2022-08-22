b6b616109f32b3ee022c0c67439a12e47663a08e
niranjan451 <84759310+niranjan451@users.noreply.github.com>
2022-08-22 15:50:28 +0530

    n=int(input()) x=0 y=1 l1=[0,1] c=2 for i in range(1,n*n):     c=c+1     z=x+y     l1.append(z)     x=y     y=z     if c==n:         break for i in l1:     print(i,end=" ")

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+n=int(input())
+x=0
+y=1
+l1=[0,1]
+c=2
+for i in range(1,n*n):
+    c=c+1
+    z=x+y
+    l1.append(z)
+    x=y
+    y=z
+    if c==n:
+        break
+for i in l1:
+    print(i,end=" ")
