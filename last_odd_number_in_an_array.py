aee0264167f728086962cb7915901c57b024c1a6
niranjan451 <84759310+niranjan451@users.noreply.github.com>
2022-08-24 18:18:40 +0530

    n=int(input()) l=list(map(int,input().split())) l1=l[::-1] for i in range(0,len(l1)):     if l1[i]%2!=0:         print(l1[i])         break     else:         continue

---
+++ last_odd_number_in_an_array.py
@@ -0,0 +1,10 @@
+n=int(input())
+l=list(map(int,input().split()))
+l1=l[::-1]
+for i in range(0,len(l1)):
+    if l1[i]%2!=0:
+        print(l1[i])
+        break
+    else:
+        continue
+        
