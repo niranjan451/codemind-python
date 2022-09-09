befdfda73e8f0815983b99e12d0677b005463df5
niranjan451 <84759310+niranjan451@users.noreply.github.com>
2022-09-09 20:37:24 +0530

    n=input() s=n.lower() v=["a","e","i","o","u"] l1=[] for i in s:     if i  in v:         if i not in l1:             l1.append(i) if len(l1)==len(v):     print(0) else:     for j in v:         if j not in l1:             print(j,end=" ")

---
+++ vowels_not_in_a_string.py
@@ -0,0 +1,15 @@
+n=input()
+s=n.lower()
+v=["a","e","i","o","u"]
+l1=[]
+for i in s:
+    if i  in v:
+        if i not in l1:
+            l1.append(i)
+if len(l1)==len(v):
+    print(0)
+else:
+    for j in v:
+        if j not in l1:
+            print(j,end=" ")
+        
