8faec8e5ab7da6b396d1e5b5b5443fb7a5a0ed8a
niranjan451 <84759310+niranjan451@users.noreply.github.com>
2022-10-06 11:15:24 +0530

    s=input() s4=s.split() s3="".join(s4) l=[] for i in s3:     l.append(ord(i)) x=min(l) y=max(l) a=l.count(x) b=l.count(y) print(chr(x),a,chr(y),b)

--- small_and_large_elements_appearences.py
+++ small_and_large_elements_appearences.py
@@ -1,4 +1,13 @@
-n=input()
-m=n.split()
-m1="".join(m)
-print(min(m1),m1.count(min(m1)),max(m1),m1.count(max(m1)))
+s=input()
+s4=s.split()
+s3="".join(s4)
+l=[]
+for i in s3:
+    l.append(ord(i))
+x=min(l)
+y=max(l)
+a=l.count(x)
+b=l.count(y)
+print(chr(x),a,chr(y),b)
+
+    
